69f91e1aba6b0eda3126ab817c6addff0540534f
Julianna <julianna.j.lim@gmail.com>
2020-07-29 20:40:59 -0400

    used node f module instad of writeFileSync function directly"

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,8 @@
 const request = require('request');
-const { writeFileSync, statSync } = require('fs');
+// const { writeFileSync, statSync } = require('fs');
+const fs = require('fs');
+
+// console.log(fs);
 
 // request with environment varible form user input at index [0]
 // get a call back to do something which is a function 
@@ -7,7 +10,8 @@ const { writeFileSync, statSync } = require('fs');
 // to write the file location from the environment var. at index[1]
 
 request(process.argv[2], function (error, result, body) {
-  writeFileSync(process.argv[3], body);
-  const file = statSync(process.argv[3]);
+  // writeFileSync(process.argv[3], body);
+  fs.writeFileSync(process.argv[3], body);
+  const file = fs.statSync(process.argv[3]);
   console.log(`Downloaded and saved ${file.size} bytes to ${process.argv[3]}`);
 });
